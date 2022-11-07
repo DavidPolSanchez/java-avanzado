e488acb4072dcc516ea674a0c6f4b6cea0b382ac
dsanchez <david.martos@amaris.com>
2022-11-07 14:33:46 +0100

    finishing exercice 8

--- src/main/java/com/example/stream/examples/sales/Example8.java
+++ src/main/java/com/example/stream/examples/sales/Example8.java
@@ -3,7 +3,9 @@ package com.example.stream.examples.sales;
 import com.example.domain.Sale;
 import com.example.utils.DataUtils;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.Comparator.comparing;
 
@@ -15,6 +17,13 @@ public class Example8 {
     public static void main(String[] args) {
 
         List<Sale> sales = DataUtils.getSales();
+
+        Optional<Integer> Customers ;
+        Customers= sales.stream()
+                .map(Sale::getValue)
+                .min(Comparator.naturalOrder());
+
+        System.out.println(Customers);
 
     }
 }
