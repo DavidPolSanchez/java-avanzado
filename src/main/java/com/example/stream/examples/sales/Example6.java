cd54216682ecd04b2c3d00f7d3f8ba428929e89e
dsanchez <david.martos@amaris.com>
2022-11-07 14:32:30 +0100

    finishing exercice 6 and 7,

--- src/main/java/com/example/stream/examples/sales/Example6.java
+++ src/main/java/com/example/stream/examples/sales/Example6.java
@@ -1,7 +1,9 @@
 package com.example.stream.examples.sales;
 
+import com.example.domain.Customer;
 import com.example.domain.Sale;
 
+import java.util.Comparator;
 import java.util.List;
 
 import static com.example.utils.DataUtils.getSales;
@@ -15,5 +17,11 @@ public class Example6 {
     public static void main(String[] args) {
 
         List<Sale> sales = getSales();
+        List<Sale> salesfiltered;
+        salesfiltered= sales.stream()
+                .filter(sale -> sale.getCustomer().getCity().equals("Cambridge"))
+                .toList();
+
+        System.out.println(salesfiltered);
     }
 }
--- src/main/java/com/example/stream/examples/sales/Example7.java
+++ src/main/java/com/example/stream/examples/sales/Example7.java
@@ -1,9 +1,12 @@
 package com.example.stream.examples.sales;
 
+import com.example.domain.Customer;
 import com.example.domain.Sale;
 import com.example.utils.DataUtils;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.Comparator.comparing;
 
@@ -15,6 +18,13 @@ public class Example7 {
     public static void main(String[] args) {
 
         List<Sale> sales = DataUtils.getSales();
+
+        Optional<Integer> Customers ;
+        Customers= sales.stream()
+            .map(Sale::getValue)
+            .max(Comparator.naturalOrder());
+
+        System.out.println(Customers);
 
     }
 }
